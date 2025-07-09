7afeb085562b50fadc2e6f1060c167ba1d92d447
Akhilesh <akhileshtiss8@gmail.com>
2025-07-09 10:10:17 +0530

    new Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,31 +4,31 @@ import { BellRing, CircleCheckBig, LayoutDashboard, LogOut, SlidersHorizontal } 
 const Navbar = () => {
   return (
     <div className='p-3 '>
-      <div className="flex flex-col justify-start bg-white p-3 rounded-3xl shadow-2xl max-w-xs">
+      <div className="flex flex-col justify-start bg-white py-3 rounded-3xl shadow-2xl min-w-xs">
         <div className="">
           <div className="flex top-1 gap-4 ml-5 items-center">
-            <p className='border px-3 py-1 rounded-full bg-green-400'>T</p>
+            <p className='px-3 py-1 rounded-full bg-green-400'>T</p>
             <p className='text-lg'>Task-Manager</p>
           </div>
-          <div className="flex gap-4 items-center py-2 px-7 shadow-2xs mt-5 ">
+          <div className="flex gap-4 items-center py-2 px-7 shadow-2xs mt-5 hover:bg-green-100 cursor-pointer">
               <LayoutDashboard />
               <p>Dashboard</p>
           </div>
-          <div className="flex gap-4 items-center py-2 px-7 shadow-xs shadow-black ">
+          <div className="flex gap-4 items-center py-2 px-7 shadow-2xs hover:bg-green-100 cursor-pointer">
               <CircleCheckBig />
               <p>My tasks</p>
           </div>
-          <div className="flex gap-4 items-center py-2 px-7 shadow-xs shadow-black ">
+          <div className="flex gap-4 items-center py-2 px-7 shadow-2xs hover:bg-green-100 cursor-pointer">
               <BellRing />
               <p>Notiications</p>
           </div>
         </div>
         <div className="bottom-1 mt-86">
-          <div className="flex gap-4 items-center py-2 px-7 ">
+          <div className="flex gap-4 items-center py-2 px-7 hover:bg-green-100 cursor-pointer">
               <SlidersHorizontal />
               <p>Settings</p>
           </div>
-          <div className="flex gap-4 items-center py-2 px-7 ">
+          <div className="flex gap-4 items-center py-2 px-7 hover:bg-green-100 cursor-pointer">
               <LogOut />
               <p>Log out</p>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import SeachSystem from '../components/SeachSystem'
 
 const Home = () => {
   return (
     <div className='min-h-screen bg-gray-200'>
-        <Navbar/>
-      <div className="">
+        <div className="flex ">
+          <Navbar/>
+          <div className="flex-col">
+            <SeachSystem/>
+            <div className="grid grid-cols-3">
+              <div className=""> {/*calendar*/}
+                <Calendar/>
+              </div> 
+              <div className="">  {/*my tasks*/}
 
-      </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
